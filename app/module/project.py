aef39ff5980210298b3f2fc979e5e39598a8283e
Dwi Purwanto <email>
2019-08-12 18:51:33 +0700

    new code for ajax support

--- app/module/project.py
+++ app/module/project.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, jsonify
 from app import app
 import pandas as pd
 import nltk
@@ -60,89 +60,107 @@ def Index():
 
 @app.route('/olah', methods=['GET', 'POST'])
 def olah():
-    query = str(request.form['query'])
-    tesData = [preProcess(query)]
-    trainData = [x for x in data_training["PreProcess"]]
-    # join test and train data to count cosim
-    newData = tesData+trainData
-    # TF.IDF vector
-    vector=TfidfVectorizer(smooth_idf=False, norm=None)
-    # limit biar gak kelihatan banyak banget
-    tfidf_matrix=vector.fit_transform(newData)
-    # cosine similiarity
-    cosim=cosine_similarity(tfidf_matrix, tfidf_matrix[0:1])
-    # save to dataframe
-    dfCosime = pd.DataFrame(cosim[1:],columns=["Cosine"]) # get cosim that not include self (1)
-    dfFinalResult = pd.concat([data_training,dfCosime],axis=1) # and concat add to train dtaframe (limit to 50 cause many)
-    # sort cosime and group by author for node
-    author = list(dict.fromkeys([str(x) for x in data_training["Author"]]))
-    save = []
-    for x in range(len(author)):
-        groupBy = dfFinalResult.loc[dfFinalResult['Author'].isin([str(author[x])])]
-        # select only have cosine value range 0-1
-        sortBy = groupBy.loc[(groupBy["Cosine"] > 0) & (groupBy["Cosine"] < 1)]
-        if(len(sortBy) != 0):  # if sortBy exist, greater than 0
-            save.append(sortBy.reset_index(drop=True))  # save
-    #only get df cosine clean
-    dfCosineClean = dfFinalResult.loc[(dfFinalResult["Cosine"]>0) & (dfFinalResult["Cosine"]<1)]
-    #get json
-    def showJSON():
-        nodes = []
-        edges = []
-        param = len(save)
-        for x in range(param):
-            kordY = int(random.randint(0, param))  # random for plot only
-            kordX = int(random.randint(0, param))  # random for plot only
-            aut = str(list(dict.fromkeys(save[x]["Author"]))[0])
-            isi_nodes = {
-                "color": hexCodeColor(),
-                "label": aut,
-                "y": kordY,
-                "x": kordX,
-                "id": aut,
-                # biar kelihatan kali 10 atau terserah
-                "size": int(round(save[x]["Cosine"].sum(axis=0)*1000, 2))
+    if request.method == 'GET':
+        msg = ['maaf halaman yang anda minta tidak dapat terpenuhi / tidak ada']
+        return render_template('page_errorhandling.html',message=msg), 512       
+    elif request.method == 'POST':
+        query = str(request.form['query']) #if use form
+        if(len(query)>0):
+            # query = str(request.json['query']) #if use other like ajax json
+            tesData = [preProcess(query)]
+            trainData = [x for x in data_training["PreProcess"]]
+            # join test and train data to count cosim
+            newData = tesData+trainData
+            # TF.IDF vector
+            vector=TfidfVectorizer(smooth_idf=False, norm=None)
+            # limit biar gak kelihatan banyak banget
+            tfidf_matrix=vector.fit_transform(newData)
+            # cosine similiarity
+            cosim=cosine_similarity(tfidf_matrix, tfidf_matrix[0:1])
+            # save to dataframe
+            dfCosime = pd.DataFrame(cosim[1:],columns=["Cosine"]) # get cosim that not include self (1)
+            dfFinalResult = pd.concat([data_training,dfCosime],axis=1) # and concat add to train dtaframe (limit to 50 cause many)
+            # sort cosime and group by author for node
+            author = list(dict.fromkeys([str(x) for x in data_training["Author"]]))
+            save = []
+            for x in range(len(author)):
+                groupBy = dfFinalResult.loc[dfFinalResult['Author'].isin([str(author[x])])]
+                # select only have cosine value range 0-1
+                sortBy = groupBy.loc[(groupBy["Cosine"] > 0) & (groupBy["Cosine"] < 1)]
+                if(len(sortBy) != 0):  # if sortBy exist, greater than 0
+                    save.append(sortBy.reset_index(drop=True))  # save
+            #only get df cosine clean
+            dfCosineClean = dfFinalResult.loc[(dfFinalResult["Cosine"]>0) & (dfFinalResult["Cosine"]<1)]
+            #get json
+            def showJSON():
+                nodes = []
+                edges = []
+                param = len(save)
+                for x in range(param):
+                    kordY = int(random.randint(0, param))  # random for plot only
+                    kordX = int(random.randint(0, param))  # random for plot only
+                    aut = str(list(dict.fromkeys(save[x]["Author"]))[0])
+                    isi_nodes = {
+                        "color": hexCodeColor(),
+                        "label": aut,
+                        "y": kordY,
+                        "x": kordX,
+                        "id": aut,
+                        # biar kelihatan kali 10 atau terserah
+                        "size": int(round(save[x]["Cosine"].sum(axis=0)*(param/10), 2))
+                    }
+                    nodes.append(isi_nodes)  # save node
+                    for y in save[x]["Judul"]:
+                        contain = dfCosineClean.loc[dfCosineClean['Judul'].isin([y])]
+                        checkCountAuthor = list(dict.fromkeys(contain["Author"]))
+                        if(len(checkCountAuthor) > 0):
+                            for z in checkCountAuthor:
+                                if aut != z:  # if same will continue
+                                    isi_edges = {
+                                        "sourceID": aut,
+                                        "targetID": z,
+                                    }
+                                    edges.append(isi_edges)  # save edges kecil
+
+                data_json = {
+                    "nodes": nodes,
+                    "edges": edges
+                }
+                return data_json
+
+            # decrypt json
+            nds=[{
+                    "x": node["x"],
+                    "y": node["y"],
+                    "id": node["id"],
+                    "name": node["label"],
+                    "symbolSize": node["size"],
+                    "value": node["size"],
+                    "itemStyle": {"normal": {"color": node["color"]}},
+                } for node in showJSON()["nodes"]]
+
+            edgs=[{
+                    "source": edge["sourceID"],
+                    "target": edge["targetID"],
+                } for edge in showJSON()["edges"]]
+            # return jsonify({'data':render_template("hasil_olah.html", node=nds, edge=edgs)}) #if not via ajax json use this render template
+            # return render_template("hasil_olah.html", node=nds, edge=edgs) #if not via ajax json use this render template
+            data = {
+                'error':False,
+                'edge':edgs,
+                'node':nds
             }
-            nodes.append(isi_nodes)  # save node
-            for y in save[x]["Judul"]:
-                contain = dfCosineClean.loc[dfCosineClean['Judul'].isin([y])]
-                checkCountAuthor = list(dict.fromkeys(contain["Author"]))
-                if(len(checkCountAuthor) > 0):
-                    for z in checkCountAuthor:
-                        if aut != z:  # if same will continue
-                            isi_edges = {
-                                "sourceID": aut,
-                                "targetID": z,
-                            }
-                            edges.append(isi_edges)  # save edges kecil
-
-        data_json = {
-            "nodes": nodes,
-            "edges": edges
-        }
-        return data_json
-
-    # decrypt json
-    nds=[{
-            "x": node["x"],
-            "y": node["y"],
-            "id": node["id"],
-            "name": node["label"],
-            "symbolSize": 2,
-            "value": node["size"],
-            "itemStyle": {"normal": {"color": node["color"]}},
-        } for node in showJSON()["nodes"]]
-
-    edgs=[{
-            "source": edge["sourceID"],
-            "target": edge["targetID"],
-        } for edge in showJSON()["edges"]]
-    return render_template("hasil_olah.html", node=nds, edge=edgs)
-
+            return jsonify(data)
+        else:
+            return jsonify({"error":True,'edge':[],'node':[]})
+    else:
+        msg = ['maaf request anda tidak dapat kami penuhi']
+        return render_template('page_errorhandling.html', message=msg), 302
 @app.route('/index', methods=['GET', 'POST'])
 def Indeks():
     return redirect(url_for("Index"))
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('page_not_found.html'), 404
+    msg = ['maaf halaman yang anda minta tidak dapat terpenuhi / tidak ada']
+    return render_template('page_errorhandling.html',message=msg), 404
