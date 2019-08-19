a26f15044121dad00f9e62b4bba05a1ed4c9304e
Dwi Purwanto <email>
2019-08-19 07:00:15 +0700

    fiz issue coordinat for fuzzy method

--- app/module/project.py
+++ app/module/project.py
@@ -8,9 +8,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 from sklearn.metrics.pairwise import cosine_similarity
+from fuzzywuzzy import fuzz
 stemmer = StemmerFactory().create_stemmer()
 remover = StopWordRemoverFactory().create_stop_word_remover()
-katabaku = pd.read_csv('app/data/vocab_katabaku.csv')
+katabaku = pd.read_csv("app/data/vocab_katabaku.csv")
 data_training = pd.read_csv("app/data/finalPrerapi.csv")
 baku = [x for x in katabaku["kata_baku"]]
 
@@ -54,54 +55,50 @@ def hexCodeColor():
     return "#" + z.upper()
 
 #get json
-def showJSON(save):
+def showJSON(save,choice):
     scat = []
     param = len(save)
-    pos_nums = []
-    konstanta = []
-    c=1
-    param2=param
-    while param2 != 0:
-        z = param2 % 10
-        pos_nums.append(z * c) # if wanna find number arrange
-        param2 = param2 // 10
-        c = c*10
-        konstanta.append(round(c/10))
-    # bulat = pos_nums[len(pos_nums)-1:]
-    kons = konstanta[len(konstanta)-1:]
+    pilih = choice
     for x in range(param):
         kordX = int(random.randint(0, param))  # random for plot only
         aut = str(list(dict.fromkeys(save[x]["Author"]))[0])
-        nilai_cosim = save[x]["Cosine"].sum(axis=0)
+        if(pilih=="fuzzy"):
+            nilai = save[x]["TSR"].sum(axis=0)
+            size = len(save[x]["TSR"])*10
+            kordY = float(round(nilai))
+        elif(pilih=="cosine"):
+            nilai = save[x]["Cosine"].sum(axis=0)
+            size = len(save[x]["Cosine"])*10
+            kordY = float(round(nilai,2))
+        else:
+            return "maaf module lain belum ada"
         isi = {
             "color": hexCodeColor(),
             "label": aut,
-            "y": round(nilai_cosim,2),
+            "y": kordY,
             "x": kordX,
             # biar kelihatan kali 10 atau terserah
-            "size": round(nilai_cosim,2)*100
+            # "size": round(nilai_cosim, 2)*100
+            # "size": random.uniform(0, round(nilai_cosim)*100)
+            "size": size
         }
         scat.append(isi)  # save node
+    return scat
 
-    data_json = {
-        "scatter": scat,
-    }
-    return data_json
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def Index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/olah', methods=['GET', 'POST'])
+@app.route("/olah", methods=["GET", "POST"])
 def olah():
-    if request.method == 'GET':
-        msg = ['maaf halaman yang anda minta tidak dapat terpenuhi / tidak ada']
-        return render_template('page_errorhandling.html',message=msg), 512       
-    elif request.method == 'POST':
-        query = str(request.form['query']) #if use form
+    if request.method == "GET":
+        msg = ["maaf halaman yang anda minta tidak dapat terpenuhi / tidak ada"]
+        return render_template("page_errorhandling.html",message=msg), 512       
+    elif request.method == "POST":
+        query = str(request.form["query"]) #if use form
         if(len(query)>0):
-            # query = str(request.json['query']) #if use other like ajax json
+            # query = str(request.json["query"]) #if use other like ajax json
             tesData = [preProcess(query)]
             trainData = [x for x in data_training["PreProcess"]]
             # join test and train data to count cosim
@@ -115,19 +112,32 @@ def olah():
             # save to dataframe
             dfCosime = pd.DataFrame(cosim[1:],columns=["Cosine"]) # get cosim that not include self (1)
             dfFinalResult = pd.concat([data_training,dfCosime],axis=1) # and concat add to train dtaframe (limit to 50 cause many)
-            # sort cosime and group by author for node
+            # for fuzzy method
+            saveFuzzy = []
+            queryFuzzy = str(tesData)
+            for j in range(len(trainData)):
+                # karena dicocokkan dengan yang sudah ke pre-proces, sebenarnya tidak dipre-process tidak apa-apa cuma biar lebih clean saja :D
+                tokenSetRatio = fuzz.token_set_ratio(queryFuzzy, str(trainData[j]))
+                saveFuzzy.append([str(data_training["Judul"][j]), str(data_training["Author"][j]), tokenSetRatio])
+            dfTSR = pd.DataFrame(saveFuzzy, columns=["Judul", "Author", "TSR"])  # Token Set Ratio
+            # sort cosime and tsr for group by author in node
             author = list(dict.fromkeys([str(x) for x in data_training["Author"]]))
-            save = []
+            sortGroupCos = []
+            sortGroupFuzz = []
             for x in range(len(author)):
-                groupBy = dfFinalResult.loc[dfFinalResult['Author'].isin([str(author[x])])]
+                groupBy = dfFinalResult.loc[dfFinalResult["Author"].isin([str(author[x])])]
+                groupBy2 = dfTSR.loc[dfTSR["Author"].isin([str(author[x])])]
                 # select only have cosine value range 0-1
-                sortBy = groupBy.loc[(groupBy["Cosine"] > 0) & (groupBy["Cosine"] < 1)]
+                sortBy = groupBy.loc[(groupBy["Cosine"] > 0) & (groupBy["Cosine"] < 1)] #all cosine except 0, maybe this can
+                sortBy2 = groupBy2.loc[groupBy2["TSR"]>40] #value want to use similiar match string grater tahn 50%
                 if(len(sortBy) != 0):  # if sortBy exist, greater than 0
-                    save.append(sortBy.reset_index(drop=True))  # save
+                    sortGroupCos.append(sortBy.reset_index(drop=True))  # save
+                if(len(sortBy2) != 0):
+                    sortGroupFuzz.append(sortBy2.reset_index(drop=True))
             # decrypt json
-            isi_scatter = [{
+            isi_scatterCos = [{
                 "name": sc["label"],
-                "type": 'scatter',
+                "type": "scatter",
                 "data": [[sc["x"], sc["y"]]],
                 "symbolSize": sc["size"],
                 "label": {
@@ -139,22 +149,36 @@ def olah():
                     "opacity": 0.4,
                     "normal": {"color": sc["color"]}
                 }
-            } for sc in showJSON(save)["scatter"]]
-            data = {
-                'error':False,
-                'scatter':isi_scatter,
-            }
-            return jsonify(data)
+            } for sc in showJSON(sortGroupCos,"cosine")] # 0 is cosine
+
+            isi_scatterFuzz = [{
+                "name": sc["label"],
+                "type": "scatter",
+                "data": [[sc["x"],sc["y"]]],
+                "symbolSize": sc["size"],
+                "label": {
+                    "show": "true",
+                    "position": "top",
+                    "formatter": "{a}"
+                },
+                "itemStyle": {
+                    "opacity": 0.4,
+                    "normal": {"color": sc["color"]}
+                }
+            } for sc in showJSON(sortGroupFuzz,"fuzzy")]  # 1 or grater is tsr
+            print(len(isi_scatterCos))
+            print(len(isi_scatterFuzz))
+            return jsonify({"error":False,"scatterCos":isi_scatterCos,"scatterFuzz":isi_scatterFuzz})
         else:
-            return jsonify({"error":True,'scatter':[]})
+            return jsonify({"error":True,"scatterCos":[],"scatterFuzz":[]})
     else:
-        msg = ['maaf request anda tidak dapat kami penuhi']
-        return render_template('page_errorhandling.html', message=msg), 302
-@app.route('/index', methods=['GET', 'POST'])
+        msg = ["maaf request anda tidak dapat kami penuhi"]
+        return render_template("page_errorhandling.html", message=msg), 302
+@app.route("/index", methods=["GET", "POST"])
 def Indeks():
     return redirect(url_for("Index"))
 
 @app.errorhandler(404)
 def page_not_found(error):
-    msg = ['maaf halaman yang anda minta tidak dapat terpenuhi / tidak ada']
-    return render_template('page_errorhandling.html',message=msg), 404
+    msg = ["maaf halaman yang anda minta tidak dapat terpenuhi / tidak ada"]
+    return render_template("page_errorhandling.html",message=msg), 404
